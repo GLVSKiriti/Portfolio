956a4fa03e01c85c2f6cc955869ce195915a5acc
GLVSKiriti <glvskiriti2003369@gmail.com>
2023-08-19 00:48:21 +0530

    Added favicon, logo and navbar links constant
    Signed-off-by: GLVSKiriti <glvskiriti2003369@gmail.com>

---
+++ src/constants/index.ts
@@ -0,0 +1,28 @@
+const navBarLinks = [
+  {
+    id: 1,
+    title: "About",
+  },
+  {
+    id: 2,
+    title: "Skills",
+  },
+  {
+    id: 3,
+    title: "Experience",
+  },
+  {
+    id: 4,
+    title: "Projects",
+  },
+  {
+    id: 5,
+    title: "Blog",
+  },
+  {
+    id: 6,
+    title: "Contact",
+  },
+];
+
+export { navBarLinks };
