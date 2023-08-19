0b0a15c1e623e49f66851a881dd3751fef069584
GLVSKiriti <glvskiriti2003369@gmail.com>
2023-08-19 16:30:31 +0530

    Made the navBar Responsive
    Signed-off-by: GLVSKiriti <glvskiriti2003369@gmail.com>

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,14 @@
 import "./NavBar.css";
 import logo from "../assets/kiriti-high-resolution-logo-color-on-transparent-background.png";
+import menu from "../assets/icons8-hamburger-menu-48.png";
+import cross from "../assets/icons8-cross-48.png";
 import { navBarLinks } from "../constants/index.ts";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function NavBar() {
+  const [activeLink, setActiveLink] = useState("");
+  const [toggle, setToggle] = useState(false);
   return (
     <>
       <div className="navBar">
@@ -11,9 +17,47 @@ function NavBar() {
         </div>
         <ul className="navBarList">
           {navBarLinks.map((each) => (
-            <li>{each.title}</li>
+            <Link
+              to={"/" + each.title}
+              style={{ color: "white", textDecoration: "none" }}
+            >
+              <li
+                key={each.id}
+                className={activeLink === each.title ? "list active" : "list"}
+                onClick={() => setActiveLink(each.title)}
+              >
+                {each.title}
+              </li>
+            </Link>
           ))}
         </ul>
+        <div className="hamBurgerMenu">
+          <img
+            src={!toggle ? menu : cross}
+            alt="menu"
+            onClick={() => setToggle(!toggle)}
+          />
+          <div className={!toggle ? "dropDownMenu hidden" : "dropDownMenu"}>
+            <ul className="navBarList">
+              {navBarLinks.map((each) => (
+                <Link
+                  to={"/" + each.title}
+                  style={{ color: "white", textDecoration: "none" }}
+                >
+                  <li
+                    key={each.id}
+                    className={
+                      activeLink === each.title ? "list active" : "list"
+                    }
+                    onClick={() => setActiveLink(each.title)}
+                  >
+                    {each.title}
+                  </li>
+                </Link>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </>
   );
