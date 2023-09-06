c7a387db4e264ccab4e6f7735fd050c886be47f3
GLVSKiriti <glvskiriti2003369@gmail.com>
2023-09-06 18:20:39 +0530

    Blogs section is done
    Signed-off-by: GLVSKiriti <glvskiriti2003369@gmail.com>

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -3,7 +3,14 @@ import "./Blogs.scss";
 function Blogs() {
   return (
     <div className="blogs" id="Blog">
-      My Blogs
+      <h1 className="heading">Blog</h1>
+      <p className="content">
+        I'm an enthusiastic technical blog writer who loves breaking down
+        complex concepts into beginner-friendly articles. I also share my
+        real-world working experiences through my blog. Feel free to explore my
+        blog page for valuable insights.
+      </p>
+      <button className="blogButton">Read Here</button>
     </div>
   );
 }
